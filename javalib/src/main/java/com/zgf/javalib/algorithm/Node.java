2029351b68485d91f6ba3d851822ab889c82aac2
zgfei <m18317713325@163.com>
2021-03-30 21:25:55 +0800

    添加链表常见算法

--- javalib/src/main/java/com/zgf/javalib/algorithm/Node.java
+++ javalib/src/main/java/com/zgf/javalib/algorithm/Node.java
@@ -1,5 +1,7 @@
 package com.zgf.javalib.algorithm;
 
+import java.util.Objects;
+
 public class Node<T> {
     T val;
     Node<T> next;
@@ -15,5 +17,19 @@ public class Node<T> {
     public Node(T t, Node<T> n) {
         val = t;
         next = n;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(val, node.val) &&
+                Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
     }
 }
--- javalib/src/main/java/com/zgf/javalib/algorithm/NodeAlgorithm2.java
+++ javalib/src/main/java/com/zgf/javalib/algorithm/NodeAlgorithm2.java
@@ -1,5 +1,7 @@
 package com.zgf.javalib.algorithm;
 
+import java.util.HashSet;
+
 class NodeAlgorithm2 {
     public static void main(String[] args) {
         Node<Integer> node = createNode(7);
@@ -68,9 +70,49 @@ class NodeAlgorithm2 {
 
         Node<Integer> node11 = createNode(5);
         printNode(node11);
-        Node<Integer> swapRangeNode = swapRangeNode(node11, 3, 4);
+        Node<Integer> swapRangeNode = swapRangeNode(node11, 2, 4);
         printNode(swapRangeNode);
         System.out.println("============swapRangeNode============");
+
+        Node<Integer> node12 = createNode(1, 2, 3, 4, 5, 6);
+        Node<Integer> node13 = createNode(7, 6, 8, 0);
+        printNode(node12);
+        printNode(node13);
+        findIntersectNode(node12, node13);
+        System.out.println("============findIntersectNode============");
+
+        Node<Integer> node14 = createNode(1, 2, 3, 4, 5, 6);
+        Node<Integer> node15 = createNode(7, 6, 8, 0);
+        printNode(node14);
+        printNode(node15);
+        Node<Integer> intersectNode2 = findIntersectNode2(node14, node15);
+        System.out.println("============findIntersectNode==222==========" + intersectNode2.val);
+
+        Node<Integer> node16 = createNode(1, 2, 3, 4, 5, 6);
+        printNode(node16);
+        Node<Integer> lastNode = getLastNode(node16);
+        System.out.println("===lastNode====" + lastNode.val);
+        Node<Integer> getNode = getNode(node16, 2);
+        System.out.println("===getNode====" + getNode.val);
+        lastNode.next = getNode;
+        Node<Integer> detectCycle = detectCycle(node16);
+        System.out.println("============detectCycle============" + detectCycle.val);
+
+        Node<Integer> node17 = createNode(1, 2, 3, 4, 5, 6);
+        printNode(node17);
+        Node<Integer> lastNode17 = getLastNode(node17);
+        System.out.println("===lastNode====" + lastNode17.val);
+        Node<Integer> getNode17 = getNode(node17, 0);
+        System.out.println("===getNode====" + getNode17.val);
+        lastNode17.next = getNode17;
+        Node<Integer> detectCycle2 = detectCycle2(node17);
+        System.out.println("============detectCycle====222========" + detectCycle2.val);
+
+        Node<Integer> node18 = createNode(3, 2, 1, 4, 6, 5);
+        printNode(node18);
+        Node<Integer> sortNode = sortNode(node18);
+        printNode(sortNode);
+        System.out.println("============sortNode============");
     }
 
     private static Node<Integer> createNode(int... datas) {
@@ -114,6 +156,22 @@ class NodeAlgorithm2 {
         System.out.println();
     }
 
+    private static Node<Integer> getLastNode(Node<Integer> node) {
+        Node<Integer> head = node;
+        while (head != null && head.next != null) {
+            head = head.next;
+        }
+        return head;
+    }
+
+    private static Node<Integer> getNode(Node<Integer> node, int index) {
+        Node<Integer> head = node;
+        for (int i = 0; i < index && head != null; i++) {
+            head = head.next;
+        }
+        return head;
+    }
+
     /**
      * 删除链表中倒数的第n个数，然后返回链表的头
      * <p>
@@ -149,6 +207,14 @@ class NodeAlgorithm2 {
         return result;
     }
 
+    /**
+     * 双指针
+     *
+     * 先让第一个指针走n步，让后两个指针一起走，
+     * 当第一个指针走到尾部的时候，第二个指针刚好在倒数第n的位置
+     *
+     * 这里要找到倒数第n+1的位置，做删除操作
+     */
     private static Node<Integer> deleteNodeNData2(Node<Integer> node, int n) {
         // 创建一个链表头结点的前的一个节点
         Node<Integer> dummy = new Node(0, node);
@@ -267,7 +333,6 @@ class NodeAlgorithm2 {
      * 2
      * 5, 6, 1, 2, 3, 4, null
      * <p>
-     * n超过5还不支持
      */
     private static Node<Integer> spinNodeRight(Node<Integer> node, int n) {
         Node<Integer> end = node;
@@ -375,12 +440,178 @@ class NodeAlgorithm2 {
      * 1, 2, 3, 4, 5
      * 2, 3
      * 1, 3, 2, 4, 5
+     * <p>
+     * 头插法
      */
     private static Node<Integer> swapRangeNode(Node<Integer> node, int m, int n) {
-        System.out.println("=====" + m + "====" + n);
-        
+        Node<Integer> left = node;
+
+        // 找到需要反转节点的前一个节点
+        for (int i = 1; i < m - 1; i++) {
+            left = left.next;
+        }
+
+        // 找到第一个要反转的节点
+        Node<Integer> first = left.next;
+
+        for (int i = 0; i < n - m; i++) {
+            Node<Integer> next = first.next;
+            first.next = first.next.next;
+            next.next = left.next;
+            left.next = next;
+        }
 
         return node;
+    }
+
+    /**
+     * 找两个链表相交的节点，并返回每个链表当前节点的前面有几个节点和当前相交的值
+     * <p>
+     * 1, 2, 3, 4, 5, 6
+     * 7, 4, 8, 9, 0
+     * <p>
+     * 4
+     * 3, 1
+     */
+    private static void findIntersectNode(Node<Integer> one, Node<Integer> two) {
+        int o = 0, t = 0, v = -1;
+        while (one != null) {
+            Node<Integer> twoNode = two;
+            t = 0;
+            while (twoNode != null) {
+                if (one.val.equals(twoNode.val)) {
+                    v = one.val;
+                    break;
+                }
+                twoNode = twoNode.next;
+                t++;
+            }
+            if (v != -1) {
+                break;
+            }
+            one = one.next;
+            o++;
+        }
+
+        System.out.println("====v===" + v + "==o==" + o + "==t==" + t);
+    }
+
+    private static Node<Integer> findIntersectNode2(Node<Integer> headA, Node<Integer> headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        Node<Integer> pA = headA, pB = headB;
+        while (pA.val != pB.val) {
+            pA = pA.next == null ? headB : pA.next;
+            pB = pB.next == null ? headA : pB.next;
+        }
+
+        return pA;
+    }
+
+    /**
+     * 判断链表是否有环
+     * <p>
+     * 1, 2, 3, 4, 5, 2
+     * 2
+     * 利用HashSet来判断，HashSet不能保存相同的元素
+     */
+    private static Node<Integer> detectCycle(Node<Integer> node) {
+        Node<Integer> head = node;
+        HashSet<Node<Integer>> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            } else {
+                set.add(head);
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    /**
+     * 判断链表是否有环
+     *
+     * 双指针法，快慢指针
+     * <p>
+     * 1、先快慢指针找到相交节点
+     * 2、再用相同指针找到环的入口节点
+     * <p>
+     * 快指针fast和慢指针slow
+     * 快指针每次走两步，慢指针每次走一步，因此fast = 2slow
+     * 两个指针相遇的时候，快指针走了fast = slow + nb（b为环的一圈长度，n为圈数，因为相遇了，所以一定相等）
+     * fast = 2slow -> fast = slow + nb -> slow = nb
+     * 而环的入口节点是a + nb（a 为头结点到入口节点的距离，转了n圈之后肯定还是入口节点位置）
+     * 从头结点走a步肯定就是入口节点，而slow已经走了nb步了，因此slow再走a步也刚好到入口节点
+     * <p>
+     * 相同指针
+     * 两个指针同时从slow当前位置和头节点的位置走a步，相交的节点就是入口节点
+     */
+    private static Node<Integer> detectCycle2(Node<Integer> node) {
+        Node<Integer> fast = node;
+        Node<Integer> slow = node;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast.val == slow.val) {
+                break;
+            }
+        }
+        fast = node;
+        while (slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
+    /**
+     * 排序链表
+     *
+     * 归并排序
+     * <p>
+     * 1, 3, 6, 2, 4, 5
+     * 1, 2, 3, 4, 5, 6
+     */
+    private static Node<Integer> sortNode(Node<Integer> head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 快慢指针找到中间节点
+        Node<Integer> fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 将链表分成两段
+        Node<Integer> temp = slow.next;
+        slow.next = null;
+
+        // 对两段链表分别做递归操作，进行拆分
+        Node<Integer> left = sortNode(head);
+        Node<Integer> right = sortNode(temp);
+
+        // 创建一个空的头结点，用于最后返回使用
+        Node<Integer> h = new Node<>(0);
+        Node<Integer> res = h;
+
+        // 合并，用h将各个节点拼接起来
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        // 判断最后一个是否都已经拼接上
+        h.next = left != null ? left : right;
+
+        return res.next;
     }
 
 }
